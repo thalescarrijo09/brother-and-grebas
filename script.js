86f395313ae96dc711ec3194602b3f168a3f7a24
thalecarrijo09 <thales.carrijo@gmail.com>
2026-04-29 09:43:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -630,8 +630,4 @@ function formatarData(d) {
   const [ano, mes, dia] = d.split('-');
   return `${dia}/${mes}/${ano}`;
 }
-function escapeHtml(str) {
-  if (str == null) return '';
-  return String(str).replace(/&/g,'&amp;').replace(/</g,'&lt;')
-    .replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/'/g,'&#039;');
-}
+
